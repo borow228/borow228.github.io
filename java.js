bd7c6629e48004a83a69c17aa06fbf63c6ddd00f
borow228 <qeqw.eqeq@mail.ru>
2018-10-11 23:48:02 +0300

    da

---
+++ java.js
@@ -0,0 +1,235 @@
+function c1() {
+    var c1 = document.getElementById("n1");
+        w1.innerHTML = 'По пути в школу тебя позвали мимо проходящие бомжи,бухать!';
+        t1.innerHTML = '1) Пойду с мужиками! ';
+        t2.innerHTML = '2) Пойду в школу!';
+    knopki.innerHTML = '<button id="n2" onclick="c2()">1</button>,<button id="b2" onclick="k2()">2</button>';
+}
+function k1(){
+    var k1 = document.getElementById("b1");
+    w1.innerHTML = 'Идя в бухать в гараж ты увидел бомжиху которая спала на траве возле кустов!Трахнуть её?';
+    t1.innerHTML = '1) ФУууу О_о .Лучше сделаю с ней фото и в инстаграм залляю!';
+    t2.innerHTML = '2) Оууу май Конечно такую сучку пропускать!';
+    knopki.innerHTML = '<button id="n11" onclick="c11()">1</button>,<button id="b11" onclick="k11()">2</button>';
+}
+function c11() {
+    var c11 = document.getElementById("n11");
+    w1.innerHTML = 'Как только вы подоши к ней,она простнулась и укусила вас!Поздравляем у вас СПИД!';
+    t1.innerHTML = '1) Дать ей с ноги в голову и пойти бухать в гараж!';
+    t2.innerHTML = '2) Убежать и пойти в больничку!';
+    knopki.innerHTML = '<button id="n2" onclick="c2()">1</button>,<button id="b12" onclick="k12()">2</button>';
+}
+function k12() {
+    var k12 = document.getElementById("b12");
+    w1.innerHTML = 'Как только вы подбежали к больнице вас задавила скорая помощ!(Ну да пихуй у тебя же спид рано или поздно памер бы!)';
+    t1.innerHTML = 'Game';
+    t2.innerHTML = 'Over!';
+    knopki.innerHTML = '<button id="nwwwwww2" onclick="cwwwwww2()">1</button>,<button id="b1wwww2" onclick="kwwww12()">2</button>';
+}
+function k11(){
+    var k11 = document.getElementById("b11");
+    w1.innerHTML = 'Вы достали пузырь и разбудив её вы набухались и поебались в кустах!Поздравляем у вас СПИД!';
+    t1.innerHTML = '1) Пойти в гараж!';
+    t2.innerHTML = '2) Остаться с ней!';
+    knopki.innerHTML = '<button id="n2" onclick="c2()">1</button>,<button id="b22" onclick="k22()">2</button>';
+}
+function k11() {
+    var k22 = document.getElementById("b22");
+    w1.innerHTML = 'Вы остались и поебались еще пару раз!У вас болит, голова оно говорит что у нее есть таблетки от головы!';
+    t1.innerHTML = '1) Веруться домой!';
+    t2.innerHTML = '2) Пойти с ней в подвал?';
+    knopki.innerHTML = '<button id="n33" onclick="c33()">1</button>,<button id="b33" onclick="k33()">2</button>';
+}
+function c33() {
+    var c33 = document.getElementById("n33");
+    w1.innerHTML = 'Идя по дороге домой вас сбила машина!';
+    t1.innerHTML = 'Game';
+    t2.innerHTML = 'Over';
+    knopki.innerHTML = '<button id="n34242" onclick="c434332422()">1</button>,<button id="b23243242" onclick="k242342342()">2</button>';
+
+}
+function k33() {
+    var k33 = document.getElementById("b33");
+    w1.innerHTML = 'Как тоько вы зашли в подвал вас пырнул её бывший и в умерли!';
+    t1.innerHTML = 'Game';
+    t2.innerHTML = 'Over';
+    knopki.innerHTML = '<button id="n34242" onclick="c434332422()">1</button>,<button id="b23243242" onclick="k242342342()">2</button>';
+}
+function c2() {
+    var с2 = document.getElementById("n2");
+    w1.innerHTML = 'Вы пошли в гараж!Как ты будешь пить?';
+    t1.innerHTML = '1) Набухаюсь в дрова как МУЖИК!';
+    t2.innerHTML = '2) Буду пить как девочка!';
+    knopki.innerHTML = '<button id="n3" onclick="c3()">1</button>,<button id="b3" onclick="k3()">2</button>';
+}
+function k3() {
+    var k3 = document.getElementById("b3");
+    w1.innerHTML = 'По скольку вы не умеете пить вы все равно накидалист хорошо так,вы заслужили уважение бомжов.Но вы не выдержали и облювали всех и вся!Что будешь делать?';
+    t1.innerHTML = '1)Дам  на пузырь';
+    t2.innerHTML = '2) Буду драться!';
+    knopki.innerHTML = '<button id="n23" onclick="c23()">1</button>,<button id="b23" onclick="k23()">2</button>';
+}
+
+
+
+
+
+
+
+
+function k2() {
+    w1.innerHTML = 'Ты увидел девочу с твоей школы которая тебе наравиться!';
+    t1.innerHTML = '1) Подойти к ней  позроваться и пойти с ней в школу !';
+    t2.innerHTML = '2) Пойти за ней вдыхая её араматы(мб газики)!';
+    knopki.innerHTML = '<button id="n3s" onclick="c3s()">1</button>,<button id="b3s" onclick="k3s()">2</button>';
+}
+function k3s() {
+    w1.innerHTML = 'Вот ты в школе.Ты увидел что она пошла в женскую гардеробную (у нее физ-ра)';
+    t1.innerHTML = '1) Пойти за ней и подсмотреть за ней';
+    t2.innerHTML = '2) Ити на уроки(у тебя контрольная)';
+    knopki.innerHTML = '<button id="n31s" onclick="c31s()">1</button>,<button id="b31s" onclick="k31s()">2</button>';
+}
+function k31s(){
+    w1.innerHTML = 'Вот ты уже и на уроке пишишь контрольную в голове только она';
+    t1.innerHTML = '1) Продолжать думать о ней!';
+    t2.innerHTML = '2) Делать уже контрольную';
+    knopki.innerHTML = '<button id="n3s1s" onclick="c32ss()">1</button>,<button id="b31ss" onclick="k32ss()">2</button>';
+}
+function c31s() {
+    w1.innerHTML = 'Вот ты и смотришь как она переодиваеться она уже стоит голая и у тебя уже тоже стои(тебя никто не видел и не выйдет)';
+    t1.innerHTML = '1) Остаться и попытаться расчехлить свой ключ на 9';
+    t2.innerHTML = '2) Уйти на уроки(контрольняа все же)';
+    knopki.innerHTML = '<button id="n21s" onclick="c31ss()">1</button>,<button id="b21s" onclick="k31s()">2</button>';
+}
+function c32ss() {
+    w1.innerHTML = 'У тебя в голове слишком много мыслей он ней.Твоя голова разорвалась такая тупая смерть!';
+    t1.innerHTML = 'Game';
+    t2.innerHTML = 'Over!';
+    knopki.innerHTML = '<button id="nwew21s" onclick="c3wew2s()">1</button>,<button id="bwew21s" onclick="k32wews()">2</button>';
+}
+function k32ss() {
+    w1.innerHTML = 'Ты все сделал и положил тетрадь на стол';
+    t1.innerHTML = '1) Отпроситься в туалет';
+    t2.innerHTML = '2) Не нажимать!';
+    knopki.innerHTML = '<button id="i1" onclick="i1()">1</button>,<button id="u1" onclick="u1()">2</button>';
+}
+function u1() {
+    w1.innerHTML = 'Что за звуки подумал ты! И через миг упал двигун лiтака на школу пробив крышу ,а так как ты был на последнем этаже школы двыгун упал на вас!(Никто не пострадал умер только ты)';
+    t1.innerHTML = 'Game';
+    t2.innerHTML = 'Over!';
+    knopki.innerHTML = '<button id="isdsd1" onclick="iasdas1()">1</button>,<button id="usadas1" onclick="uasda1()">2</button>';
+}
+function i1() {
+    w1.innerHTML = 'Ты пошел караулить подругу в гардеробе';
+    t1.innerHTML = '1) Спрятаться в гардеробе у шкафу за вещами';
+    t2.innerHTML = '2) Передумать и пойти в класс';
+    knopki.innerHTML = '<button  onclick="u2()">1</button>,<button onclick="u1()">2</button>';
+}
+function u2() {
+    w1.innerHTML = 'Тебя нашла уборщица и дала тебе шваброй не рассчитала силу силу она убила тебе';
+    t1.innerHTML = 'Ну хз что ты делаешь не так';
+    t2.innerHTML = 'Попробуй еще раз!';
+    knopki.innerHTML = '<button id="iswewedsd1" onclick="iaqweqwesdas1()">1</button>,<button id="useqweadas1" onclick="uasdqweqa1()">2</button>';
+}
+
+
+
+function c3s() {
+    w1.innerHTML = 'Подходя к ней ты вступил в говно и оно сильно воняло!';
+    t1.innerHTML = '1) Остановиться вытерев говно и пойти уже за ней!';
+    t2.innerHTML = '2) Продолжать ити к ней!';
+    knopki.innerHTML = '<button onclick="k3s()">1</button>,<button onclick="c231()">2</button>';
+}
+function c231() {
+    w1.innerHTML = 'От тебя воняет! Ей это нэ падобаеться';
+    t1.innerHTML = '1) Спросить-Это от тебя воняет?';
+    t2.innerHTML = '2) Сказать что вступил в говно';
+    knopki.innerHTML = '<button onclick="d22()">1</button>,<button  onclick="s22()">2</button>';
+}
+function s22() {
+    w1.innerHTML = 'Кому это интересно? Всем похуй!';
+    t1.innerHTML = 'Гаме';
+    t2.innerHTML = 'Овер!';
+    knopki.innerHTML = '<button onclick="aga()">1</button>,<button  onclick="aga()">2</button>';
+}
+function aga() {
+    w1.innerHTML = 'Ага Пашов нахуй не тут то было!Заново давай!';
+    t1.innerHTML = 'Game over!';
+    knopki.innerHTML = '<button onclick="d2dssdsdsd2()">1</button>,<button  onclick="s2sdsd2()">2</button>';
+}
+function d22() {
+    w1.innerHTML = 'Что за тупые вопросы?Тебе так стыдно что убежал.Ты очень любишь басейны и поэтому ты пошол к ним и утопился настолько сильно ты их любил';
+    t1.innerHTML = '1) Посинел два кашлянул пукнул и сдулся!';
+    t2.innerHTML = '2) Game over!';
+    text.innerHTML = '<button onclick="dsdsd22()">1</button>,<button  onclick="ssdsd22()">2</button>';
+}
+function c31ss() {
+    w1.innerHTML = 'Как только ты достал ключа она заметила и была даже рада';
+    t1.innerHTML = '1) Ити к ней!';
+    t2.innerHTML = '2) Уйти';
+    knopki.innerHTML = '<button onclick="m1()">1</button>,<button  onclick="l1()">2</button>';
+}
+function m1() {
+    w1.innerHTML = 'Ура ты поебался';
+    t1.innerHTML = 'Game';
+    t2.innerHTML = 'Win!';
+    knopki.innerHTML = '<button onclick="msdsds1()">1</button>,<button  onclick="lsdsds1()">2</button>';
+}
+function l1() {
+    w1.innerHTML = 'Ты убежал на улицу и сен на лавочку.Но из за того что гребаный китаец не пошел на работу и сварил яйцо пар отводы образовался в облако и теперь эти облока у тебя над головой началась гроза и в тебя попала молния ты умер !(где запятые как читать?ПАЕБАТЬ!)';
+    knopki.innerHTML = '<button onclick="msывывdsds1()">1</button>,<button  onclick="lsывывыdsds1()">2</button>';
+    t1.innerHTML = 'Game';
+    t2.innerHTML = 'Win!';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function c3() {
+    var c3 = document.getElementById("n3");
+    w1.innerHTML = 'Бомжи тебя зауважали!Но ты не выдержал и обрыгал все что можно.Тебя хотят избить!Как загладишь то что сделал?';
+    t1.innerHTML = '1)Дам  на пузырь';
+    t2.innerHTML = '2) Буду драться!';
+    knopki.innerHTML = '<button id="n23" onclick="c23()">1</button>,<button id="b23" onclick="k23()">2</button>';
+}
+function c23() {
+    var c23 = document.getElementById("n23");
+    w1.innerHTML = 'Как только увидели еще одну бутылку вони вiдразу схаминулися,вытерев ебало от блювоты они продолжили бухать!';
+    t1.innerHTML = '1) Остаться с ними';
+    t2.innerHTML = '2) Встать и пойти домой';
+    knopki.innerHTML = '<button id="n223" onclick="c223()">1</button>,<button id="b213" onclick="k213()">2</button>';
+}
+function c223() {
+    var c223 = document.getElementById("n223");
+    w1.innerHTML = 'Как только вы открыли бутылку у вас остоновилось сердце!ТЫ ПАМЕР!';
+    t1.innerHTML = 'Game ';
+    t2.innerHTML = 'Over!';
+    knopki.innerHTML = '<button id="n2fdsf23" onclick="c22dsfsd3()">1</button>,<button id="b21sdfds3" onclick="k213sdfsd()">2</button>';
+}
+function k23() {
+    w1.innerHTML = 'Ты достал пузырь и кинул одному из них в голову оскольки разлетелись и поралили остальных бомжов ,а водка прижигала раны.Все стоят в стане';
+    t1.innerHTML = '1) Обокрасть нищих!';
+    t2.innerHTML = '2) Плюнуть и уйти домой';
+    knopki.innerHTML = '<button id="n123" onclick="c123()">1</button>,<button id="b213" onclick="k213()">2</button>';
+}
+function c123() {
+    t1.innerHTML = '1)...?';
+    t2.innerHTML = '2) Плюнуть и уйти домой';
+    w1.innerHTML = 'У них и так нима ничого!';
+    knopki.innerHTML = '<button id="n1wewe23" onclick="c1wewe23()">1</button>,<button id="b213" onclick="k213()">2</button>';
+}
+function k213() {
+    var k213 =  document.getElementById("b213");
+    w1.innerHTML = 'Как только ты встал ,гараж взорвался и ты умер(!';
+    t1.innerHTML = 'Game';
+    t2.innerHTML = 'Over!';
+    knopki.innerHTML = '<button id="n1wreewe23" onclick="c1wereewe23()">1</button>,<button id="b213" onclick="k211231233()">2</button>';
+}
